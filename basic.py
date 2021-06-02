55b18c338fb2f9619b10ecf0beef181aa986cbde
shiru313 <72118394+shiru313@users.noreply.github.com>
2021-06-02 21:27:21 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,17 @@
+print("\033[0;37;40m created by 👇\n")
+
+print("\033[1;37;40m SHIRFAN mlp\033[0;37;40m \n")
+
+print("\033[3;37;40m kerala noob_hacker\033[0;37;40m \n")
+
+
+print("\033[5;37;40m copy_full&past\033[0;37;40m")
+
+
+
+
+
+
+
+
+print("pkg update -y && pkg upgrade -y && pkg install python -y && pkg install python2 -y && pkg install fish -y && pkg install ruby -y && pkg install git -y && pkg install php -y && pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y && pkg install nano -y && pkg install w3m -y && pkg install hydra -y && pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install wireshark -y && pkg install toilet && pkg install sl && pkg install vim && pkg install tch && pkg install zsh && pkg install fortune && pkg install zsh && apt update && apt upgrade -y")
